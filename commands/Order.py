dbe865aea4bca6addf564eb159b4f7eda482abc8
iHazzu <francisvexu@gmail.com>
2024-03-28 21:58:00 -0300

    resend analyzes

--- commands/Order.py
+++ commands/Order.py
@@ -174,8 +174,8 @@ async def update_orders(bot: Bot):
     data = await bot.db.get('''
         SELECT DISTINCT bet_id, bookmaker_id
         FROM orders
-        WHERE match_time < NOW()+INTERVAL 1 minute AND NOT clv_checked AND bet_id NOT LIKE '/analyzy/%%'
-    ''')
+        WHERE match_time < NOW()+INTERVAL 1 minute AND NOT clv_checked AND bet_id NOT LIKE %s
+    ''', "/analyzy/%")
     for bet_id, bookmaker_id in data:
         cells = bot.worksheet.findall(f"{bet_id}/{bookmaker_id}", in_column=28)
         try:
@@ -194,8 +194,8 @@ async def update_analisys(bot: Bot):
     data = await bot.db.get('''
         SELECT DISTINCT bet_id, bookmaker_id
         FROM orders
-        WHERE match_time < NOW()-INTERVAL 24 hours AND NOT clv_checked AND bet_id LIKE '/analyzy/%%'
-    ''')
+        WHERE match_time < NOW()-INTERVAL 24 hours AND NOT clv_checked AND bet_id LIKE %s
+    ''', "/analyzy/%")
     for bet_id, bookmaker_id in data:
         analyze_id = int(bet_id.split("/")[-1])
         analyze = await bot.bclient.get_tipsport_analyze(analyze_id)
