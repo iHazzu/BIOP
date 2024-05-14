b4105680a9186be4bf00ddd4056892ad18b38af4
iHazzu <francisvexu@gmail.com>
2024-05-13 22:30:17 -0300

    research update

--- commands/__init__.py
+++ commands/__init__.py
@@ -19,7 +19,6 @@ class BetCog(commands.Cog):
         self.last_update_arbs_time = datetime.now(UTC) - timedelta(seconds=5)
         self.update_arbs_loop.start()
         self.update_clv_loop.start()
-        self.research_loop.start()
 
     @tasks.loop(seconds=1)
     async def update_arbs_loop(self):
